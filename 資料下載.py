1f4b201d5b5a9d378cb4e5a61b7c38db8cdf871c
Li732375 <lee732375@gmail.com>
2024-08-04 14:04:26 +0800

    Update 資料下載.py
    追加 聯邦基金利率數據

--- 資料下載.py
+++ 資料下載.py
@@ -1,6 +1,6 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
-
+import pandas_datareader.data as WebData
 
 Currency_symbol = 'TWD%3DX' # 輸入股票代號下載匯率資料
 Currency_data = yf.download(Currency_symbol, 
@@ -16,20 +16,17 @@ plt.ylabel("Closing Price") # y 軸的標籤
 plt.title(f"{Currency_symbol} Currency Price") # 圖標題
 plt.show()
 
-import pandas_datareader.data as WebData
 
 # 下載聯邦基金利率數據
 fed_funds_rate = WebData.DataReader('FEDFUNDS', 'fred', start = '2020-01-01', 
                                     end = '2024-07-30')
 # 顯示數據
-print(fed_funds_rate.describe())
-
 excel_filename = 'Fed_Funds_Rate.xlsx'
 fed_funds_rate.to_excel(excel_filename)
 print(f"匯率資料已存儲為 '{excel_filename}'")
 print(fed_funds_rate)
 
-fed_funds_rate['FEDFUNDS'].plot() # 用 CLOSE 畫出圖形
+fed_funds_rate['FEDFUNDS'].plot() # 用 FEDFUNDS 畫出圖形
 plt.xlabel("Date") # x 軸的標籤
 plt.ylabel("FEDFUNDS") # y 軸的標籤
 plt.title("Fed_Funds_Rate") # 圖標題
