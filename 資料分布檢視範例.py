3c5aad2e51f7e3dc1ea02cf5f1b7816472b8a74a
Li732375 <lee732375@gmail.com>
2024-08-07 12:32:37 +0800

    Update 資料分布檢視範例.py
    註解內容微調和追加分割資料索引回朔

--- 資料分布檢視範例.py
+++ 資料分布檢視範例.py
@@ -2,34 +2,46 @@ import pandas as pd
 from sklearn.model_selection import StratifiedKFold
 
 
-df = pd.read_excel("試跑資料TWD%3DX_Currency_data.xlsx")
+df = pd.read_excel("TWD%3DX_Currency_data.xlsx")
 
-# 計算每個標籤的比例
+# 指定欄位名稱，計算資料分布比例
 label_proportions = df['LABEL'].value_counts(normalize = True)
 print("全部資料分布：")
 print(label_proportions)
 
 print('--------------')
-# 假設資料中有一個標籤欄位（例如 '標籤'），用於保持資料分布
+
+# 指定要分割的資料標籤欄位，並且要維持資料分布
 labels = df['LABEL']
-# 設定分割數 N
-N = 4  # 例如，將資料分割成 4 筆
+
+N = 4 # 設定分割數
 
 # 初始化 StratifiedKFold
-skf = StratifiedKFold(n_splits = N, shuffle = True, 
-                      random_state = 42)
+skf = StratifiedKFold(n_splits = N, shuffle = True, random_state = 42)
+# =============================================================================
+# 參數說明
+# n_splits：指定將數據分成多少份。即交叉驗證中的折數。例如，n_splits = 3 將數據分成三個部分，每次用其中一部分作為測試集，其餘的作為訓練集。
+# 
+# shuffle：是否在分割數據前打亂數據。預設為 False，設為 True 可以打亂數據，這在處理隨機樣本時有助於提高模型的泛化能力。
+# 
+# random_state：隨機數生成的種子，用於確保每次運行代碼時結果的一致性。設置這個參數可以讓每次運行的結果一致，便於實驗重現。
+# =============================================================================
 
-# 創建一個列表來儲存分割後的 N 筆資料
-split_data = [pd.DataFrame() for _ in range(4)]
+# 創建列表儲存分割後的 N 筆資料
+split_data = [pd.DataFrame() for _ in range(N)]
+split_indices = [None for _ in range(N)]
 
 # 分割資料並保持分布
-for i, (_, test_index) in enumerate(skf.split(df, labels)):
-    split_data[i] = df.iloc[test_index]
+for i, (train_index, _) in enumerate(skf.split(df, labels)):
+    split_data[i] = df.iloc[train_index]
+    # 保存對應的原始索引
+    split_indices[i] = train_index
 
-# 檢查分割後的資料分布
-for i, split in enumerate(split_data):
-    print(f"分割資料 {i+1}：")
+# 檢查分割後的訓練集資料及其索引
+for i, (split, indices) in enumerate(zip(split_data, split_indices)):
+    print(f"訓練集資料 {i+1}：")
     print(split['LABEL'].value_counts(normalize = True))
-    split_data.to_excel(f"外匯分割資料_{i+1}.xlsx", index = False)
-
     
+    # 打印每個分割的原始索引
+    print(f"訓練集資料 {i+1} 在原始資料中的索引：")
+    print(indices)
