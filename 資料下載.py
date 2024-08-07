a0129c39672323ebc0d6950bba367bb8d93b2365
Li732375 <lee732375@gmail.com>
2024-08-07 11:23:14 +0800

    Update 資料下載.py
    TW_cpi 欄位名稱 Date 改 DATE

--- 資料下載.py
+++ 資料下載.py
@@ -106,10 +106,10 @@ TW_cpi['月份'] = TW_cpi['月份'].str.replace('月', '', regex = False) # 轉�
 # print(TW_cpi[TW_cpi.isna().any(axis = 1)]) # 顯示缺失值資料
 TW_cpi['西元年'] = TW_cpi['民國年'] + 1911
 TW_cpi = TW_cpi.drop(columns = ['民國年']) # 移除該欄位
-TW_cpi['Date'] = TW_cpi['西元年'].astype(str) + '/' + TW_cpi['月份'] + '/1  00:00:00' # 合併兩時間為新欄位
-TW_cpi['Date'] = pd.to_datetime(TW_cpi['Date']) # 將 'date_str' 欄位轉換為時間格式
+TW_cpi['DATE'] = TW_cpi['西元年'].astype(str) + '/' + TW_cpi['月份'] + '/1  00:00:00' # 合併兩時間為新欄位
+TW_cpi['DATE'] = pd.to_datetime(TW_cpi['DATE']) # 將 'date_str' 欄位轉換為時間格式
 TW_cpi = TW_cpi.drop(columns = ['西元年', '月份']) # 移除該欄位
-TW_cpi = TW_cpi.set_index(['Date']) # 設定索引
+TW_cpi = TW_cpi.set_index(['DATE']) # 設定索引
 TW_cpi = TW_cpi.sort_index()
 TW_cpi = TW_cpi.loc[Data_Time_Start : Data_Time_End]
 print(TW_cpi.head())
