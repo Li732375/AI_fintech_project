0799a10ab5a7365bafce506b0cb01f9b00a73d2c
Li732375 <lee732375@gmail.com>
2024-08-08 16:10:23 +0800

    Update 試跑lstm.py
    模型架構與參數調整

--- 試跑lstm.py
+++ 試跑lstm.py
@@ -2,13 +2,16 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
-from keras.layers import LSTM, Dense
+from keras.layers import LSTM, Dense, Dropout
+from tensorflow.keras.optimizers import Adam
 import tensorflow as tf
 import numpy as np
+import time
 
 # 加載數據集
 data = pd.read_excel('data.xlsx')
-X = data.drop(['LABEL', 'Date'], axis = 1)
+print(data.columns)
+X = data.drop(['LABEL', 'Date', 'Volume', ], axis = 1)
 y = data['LABEL']
 
 # 數據縮放
@@ -38,14 +41,17 @@ X_train, X_test, Y_train, Y_test = train_test_split(X_reshaped,
 
 # 構建LSTM模型
 model = Sequential()
-model.add(LSTM(50, input_shape = (X_train.shape[1], X_train.shape[2])))
-model.add(Dense(1, activation = 'sigmoid'))
-model.compile(optimizer = 'adam', loss = 'binary_crossentropy', 
-              metrics = ['accuracy']) # 編譯模型
+model.add(LSTM(units = 100, return_sequences = True, 
+               input_shape = (X_train.shape[1], X_train.shape[2])))
+model.add(Dense(1, activation = 'relu'))
+model.compile(optimizer = Adam(learning_rate = 0.001), 
+              loss = 'mean_squared_error', metrics = ['accuracy']) # 編譯模型
 
+start_time = time.time()
 # 訓練模型
-model.fit(X_train, Y_train, epochs = 20, batch_size = 32, 
+model.fit(X_train, Y_train, epochs = 330, batch_size = 32, 
           validation_split = 0.2)
+training_time = time.time() - start_time
 
 # 保存模型
 model_Save_Name = 'lstm_model.h5'
@@ -62,4 +68,7 @@ predictions = loaded_model.predict(X_test)
 train_loss, train_acc = model.evaluate(X_train, Y_train, verbose = 0) # 訓練集準確度計算
 test_loss, test_acc = model.evaluate(X_test, Y_test, verbose = 0) # 測試集準確度計算
 print('LSTM訓練集準確率: %.4f' % train_acc)
-print('LSTM測試集準確率: %.4f' % test_acc) # 0.5
+print('LSTM測試集準確率: %.4f' % test_acc)
+print(f"測試時間: {training_time:.2f} 秒")
+
+print(data.columns)
