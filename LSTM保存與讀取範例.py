e78ca1a61b601598e121f6976b167bd8cee9db53
Li732375 <lee732375@gmail.com>
2024-08-09 18:03:52 +0800

    Update LSTM保存與讀取範例.py
    單獨整理出調參變數

--- LSTM保存與讀取範例.py
+++ LSTM保存與讀取範例.py
@@ -5,6 +5,7 @@ from keras.models import Sequential
 from keras.layers import LSTM, Dense
 import tensorflow as tf
 import numpy as np
+import time
 
 # 加載數據集
 data = pd.read_excel('data.xlsx')
@@ -35,21 +36,27 @@ X_reshaped = np.array(X_reshaped)
 y_reshaped = np.array(y_reshaped)
 
 # 分割數據集
-X_train, X_test, y_train, y_test = train_test_split(X_reshaped, 
+X_train, X_test, Y_train, Y_test = train_test_split(X_reshaped, 
                                                     y_reshaped, 
                                                     test_size = 0.3, 
                                                     random_state = 42)
 
+units_1 = 100
+epochs = 20
+batch_size = 32
+
 # 構建LSTM模型
 model = Sequential()
-model.add(LSTM(50, input_shape = (X_train.shape[1], X_train.shape[2])))
+model.add(LSTM(units_1, input_shape = (X_train.shape[1], X_train.shape[2])))
 model.add(Dense(1, activation = 'sigmoid'))
 model.compile(optimizer = 'adam', loss = 'binary_crossentropy', 
               metrics = ['accuracy'])
 
+start_time = time.time()
 # 訓練模型
-model.fit(X_train, y_train, epochs = 20, batch_size = 32, 
+model.fit(X_train, Y_train, epochs = 300, batch_size = batch_size, 
           validation_split = 0.2)
+training_time = time.time() - start_time
 
 # 保存模型
 model_Save_Name = 'lstm_model.h5'
@@ -62,4 +69,14 @@ print(f"成功載入模型 {model_Save_Name}")
 
 # 使用加載的模型進行預測
 predictions = loaded_model.predict(X_test)
-print(predictions)
+
+train_loss, train_acc = model.evaluate(X_train, Y_train, verbose = 0) # 訓練集準確度計算
+test_loss, test_acc = model.evaluate(X_test, Y_test, verbose = 0) # 測試集準確度計算
+print('LSTM訓練集準確率: %.3f' % train_acc)
+print('LSTM測試集準確率: %.3f' % test_acc)
+print(f"測試時間: {training_time // 60:.2f} 分 {training_time:.2f} 秒")
+print(f"參數 \
+      \n'time_steps'： {time_steps} \
+      \n'units_1'： {units_1} \
+      \n'epochs'： {epochs} \
+      \n'batch_size'： {batch_size}")
