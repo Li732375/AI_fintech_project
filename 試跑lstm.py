ba1b7b656e1a54d209243dcc9b7ae2e4b9ab3b15
Li732375 <lee732375@gmail.com>
2024-08-09 18:05:19 +0800

    Update 試跑lstm.py
    獨立出參數供參考

--- 試跑lstm.py
+++ 試跑lstm.py
@@ -8,10 +8,16 @@ import tensorflow as tf
 import numpy as np
 import time
 
+
 # 加載數據集
 data = pd.read_excel('data.xlsx')
 print(data.columns)
-X = data.drop(['LABEL', 'Date', 'Volume', ], axis = 1)
+
+# =============================================================================
+# feature_names = ['FEDFUNDS', 'BOP', 'WILLR', 'WMA', 'AVGPRICE', 'D', 'LINEARREG_ANGLE', 'MA_10', 'CCI', 'Close', 'SAR', 'MA_20', 'K', 'High', 'MOM', 'WCLPRICE', 'Low', 'STDDEV', 'Bollinger Bands lower', 'MACD', 'RSI_14', 'UNRATE', 'CPIAUCNS', 'Bollinger Bands Upper', 'Open', 'CPI', 'MA_5']
+# X = data[feature_names].values
+# =============================================================================
+X = data.drop(['LABEL', 'Date', 'Volume'], axis = 1)
 y = data['LABEL']
 
 # 數據縮放
@@ -39,17 +45,22 @@ X_train, X_test, Y_train, Y_test = train_test_split(X_reshaped,
                                                     test_size = 0.3, 
                                                     random_state = 42)
 
+units_1 = 256
+adam_learning_rate = 0.001
+epochs = 350
+batch_size = 16
+
 # 構建LSTM模型
 model = Sequential()
-model.add(LSTM(units = 100, return_sequences = True, 
+model.add(LSTM(units = units_1, return_sequences = True, 
                input_shape = (X_train.shape[1], X_train.shape[2])))
 model.add(Dense(1, activation = 'relu'))
-model.compile(optimizer = Adam(learning_rate = 0.001), 
+model.compile(optimizer = Adam(learning_rate = adam_learning_rate), 
               loss = 'mean_squared_error', metrics = ['accuracy']) # 編譯模型
 
 start_time = time.time()
 # 訓練模型
-model.fit(X_train, Y_train, epochs = 330, batch_size = 32, 
+model.fit(X_train, Y_train, epochs = epochs, batch_size = batch_size, 
           validation_split = 0.2)
 training_time = time.time() - start_time
 
@@ -67,7 +78,13 @@ predictions = loaded_model.predict(X_test)
 
 train_loss, train_acc = model.evaluate(X_train, Y_train, verbose = 0) # 訓練集準確度計算
 test_loss, test_acc = model.evaluate(X_test, Y_test, verbose = 0) # 測試集準確度計算
-print('LSTM訓練集準確率: %.4f' % train_acc)
-print('LSTM測試集準確率: %.4f' % test_acc)
-print(f"測試時間: {training_time:.2f} 秒")
+print('LSTM訓練集準確率: %.3f' % train_acc)
+print('LSTM測試集準確率: %.3f' % test_acc)
+print(f"測試時間: {training_time // 60:.2f} 分 {training_time:.2f} 秒")
+print(f"參數 \
+      \n'time_steps'： {time_steps} \
+      \n'units_1'： {units_1} \
+      \n'adam_learning_rate'： {adam_learning_rate} \
+      \n'epochs'： {epochs} \
+      \n'batch_size'： {batch_size}")
 
