a5519224586f267bdd9f883ad30d140511c6bb21
michaelkuo <kuo900312@gmail.com>
2024-08-10 18:27:49 +0800

    Update 資料下載.py

--- 資料下載.py
+++ 資料下載.py
@@ -8,12 +8,11 @@ Data_Time_TW_Start = str(int(Data_Time_Start[0 : 4]) - 1911) + '-01-01'
 Data_Time_TW_End = str(int(Data_Time_End[0 : 4]) - 1911) + '-12-31'
 
 
-
 Currency_symbol = 'TWD%3DX' # 輸入股票代號下載匯率資料
 Currency_data = yf.download(Currency_symbol, 
                             start = Data_Time_Start, end = Data_Time_End) # 獲取特定日期範圍的匯率資料
 
-excel_filename = f'{Currency_symbol}_Currency_data.xlsx' # 將匯率資料存儲為 Excel 檔案，以匯率代號作為檔案名稱
+excel_filename = f'{Currency_symbol}_Currency_Data.xlsx' # 將匯率資料存儲為 Excel 檔案，以匯率代號作為檔案名稱
 Currency_data.to_excel(excel_filename)
 print(f"匯率資料已存儲為 '{excel_filename}'")
 print(Currency_data.head())
@@ -127,7 +126,6 @@ gdp_data = WebData.get_data_fred('GDP',
                                  start = Data_Time_Start, 
                                  end = Data_Time_End)
 gdp_data['USA_GDP_Rate'] = gdp_data['GDP'].pct_change() # 算出增長率
-
 excel_filename = 'USA_GDP.xlsx'
 gdp_data.to_excel(excel_filename)
 print(f"美國失業率資料已存儲為 '{excel_filename}'")
@@ -218,4 +216,48 @@ TW_rate.plot() # 畫出圖形
 plt.xlabel("Date") # x 軸的標籤
 plt.ylabel("Rate") # y 軸的標籤
 plt.title("台灣 機動利率") # 圖標題
+plt.show()
+
+
+# 消費者物價指數及其年增率 網址
+url = 'https://ws.dgbas.gov.tw/001/Upload/463/relfile/10315/2414/cpispl.xls'
+
+# pip install xlrd
+# 直接從 URL 讀取 excel 文件
+TW_cpi = pd.read_excel(url, header = 2) # 指定第三行（索引為2）作為欄位名稱
+print(TW_cpi.columns) # 檢視所有欄位
+
+TW_cpi = TW_cpi.drop(columns = ['累計平均']) # 移除該欄位
+TW_cpi = TW_cpi[:-4] # 移除最後四筆資料
+print(TW_cpi)
+
+# 轉換為長格式。將指定列變成行，並且通常是將多個列的數據合併成少數幾列
+TW_cpi = TW_cpi.melt(id_vars = '民國年', var_name = '月份', 
+                     value_name = 'CPI')
+
+# regex 參數的預設值是 True，會將要替換的字串視為正則表達式處理。
+TW_cpi['月份'] = TW_cpi['月份'].str.replace('月', '', regex = False) # 轉換月份
+
+# print(TW_cpi[TW_cpi.isna().any(axis = 1)]) # 顯示缺失值資料
+TW_cpi['西元年'] = TW_cpi['民國年'] + 1911
+TW_cpi = TW_cpi.drop(columns = ['民國年']) # 移除該欄位
+TW_cpi['Date'] = TW_cpi['西元年'].astype(str) + '/' + TW_cpi['月份'] + '/1  12:00:00 AM' # 合併兩時間為新欄位
+TW_cpi['Date'] = pd.to_datetime(TW_cpi['Date']) # 將 'date_str' 欄位轉換為時間格式
+TW_cpi = TW_cpi.drop(columns = ['西元年', '月份']) # 移除該欄位
+TW_cpi = TW_cpi.set_index(['Date']) # 設定索引
+TW_cpi = TW_cpi.sort_index()
+TW_cpi = TW_cpi.loc[Data_Time_Start : Data_Time_End]
+print(TW_cpi)
+
+
+excel_filename = 'TW_CPI.xlsx'
+TW_cpi.to_excel(excel_filename)
+print(f"台灣 消費者物價指數 資料已存儲為 '{excel_filename}'")
+
+# 顯示數據
+plt.rcParams['font.family'] = 'Microsoft JhengHei' # 設置中文字體
+TW_cpi['CPI'].plot() # 畫出圖形
+plt.xlabel("Date") # x 軸的標籤
+plt.ylabel("CPI") # y 軸的標籤
+plt.title("台灣 消費者物價-指數") # 圖標題
 plt.show()
